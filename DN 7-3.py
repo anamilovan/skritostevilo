2efdd3dbf12cc2a082083a486f4744be4c3b548e
Ana Milovan <ana.milovan6@gmail.com>
2017-09-07 18:02:52 +0200

    Domaca naloga

---
+++ DN 7-3.py
@@ -0,0 +1,6 @@
+secret = int(raw_input("Vnesi skrito stevilko!"))
+guess = int(raw_input("Ugani skrito stevilko!"))
+if secret == guess :
+    print "Bravo, to je skrita stevilka!"
+else :
+    print "Zal to ni skrita stevilka"
