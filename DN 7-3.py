ffffa8ded472010313f827be62c24b1d09f3a1df
Ana Milovan <ana.milovan6@gmail.com>
2017-09-19 21:29:30 +0200

    random stevilke

--- DN 7-3.py
+++ DN 7-3.py
@@ -1,9 +1,21 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-secret = int(raw_input("Vnesi skrito številko!"))
-guess = int(raw_input("Ugani skrito številko!"))
-if secret == guess :
-    print "Bravo, to je skrita številka!"
-else :
-    print "Zal to ni skrita številka"
+import random
+
+def main():
+
+    secret = random.randint(1,1000)
+
+    while True:
+        guess = int(raw_input("Ugani skrito številko! Je med 1 in 1000"))
+        if secret == guess :
+            print "Bravo, to je skrita številka!"
+            break
+        elif secret < guess :
+            print "Zal to ni skrita številka, skrita številka je manjša"
+        elif secret > guess :
+            print "Zal to ni skrita številka, skrita številka je večja"
+
+if __name__ == '__main__':
+        main()
